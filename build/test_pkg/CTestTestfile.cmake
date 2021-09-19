e4466232cbd6917d643dda38ad52440f9be79347
Thomas Nagy Zambo <81777426+tnagyzambo@users.noreply.github.com>
2021-09-19 11:41:46 -0400

    Delete build directory

--- build/test_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/psf/CODE/ros-test/src/test_pkg
-# Build directory: /media/psf/CODE/ros-test/build/test_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
